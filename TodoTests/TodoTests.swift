2c7360d19416b8907bdda5f5e5dd2594f0b26092
Jake Lin <jakelinau@gmail.com>
2014-09-16 17:32:02 +1000

    Initial Commit

---
+++ TodoTests/TodoTests.swift
@@ -0,0 +1,36 @@
+//
+//  TodoTests.swift
+//  TodoTests
+//
+//  Created by Jake Lin on 16/09/2014.
+//  Copyright (c) 2014 Jake Lin. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class TodoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
